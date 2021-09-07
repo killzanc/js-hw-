8fa134687caec2999bec72ca8d866b799981c9ea
Sherzod <angry5572@gmail.com>
2021-09-08 00:48:46 +0500

    alisher issue

---
+++ main.js
@@ -0,0 +1,10 @@
+// uy ishi
+var bilet = 500;
+var hotel = 250;
+var muzey = 120; 
+var summa = prompt('Nech pullingiz bor(somda)?');
+if (summa >= (bilet * 9433.34) && summa >= (hotel * 9433.34) && summa >= (muzey * 10354.03)){
+    alert(`Oq yo'l`);
+}else{
+    alert(`Alisher, ozgina sabr qilish kerak bo'lar ekan`);
+}
